edb52ced0829c4e56d0916e9238cb975ce24ca68
donghyeon shin <donghyun4591@gmail.com>
2024-02-07 18:11:08 +0900

    ec2 보안그룹 테스트

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -28,3 +28,53 @@ resource "aws_instance" "ubuntu_instance" {
     Name = "App_Server"
     }
 }
+
+resource "aws_security_group" "ec2_security_group" {
+    vpc_id = aws_vpc.main_vpc.id
+
+    #기본 ssh Port
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    # SKB Internet Port
+    ingress {
+        from_port   = 11117
+        to_port     = 11117
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+    # jenkins Port
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    
+    #http Port
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "ec2-security-group"
+    }
+}
+
