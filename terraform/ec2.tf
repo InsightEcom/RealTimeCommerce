a0fc75710a3e33029dec5b6acff798a6f5715161
donghyeon shin <donghyun4591@gmail.com>
2024-02-07 17:51:49 +0900

    docker install, jenkins pull OK

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -5,17 +5,17 @@ resource "aws_instance" "ubuntu_instance" {
 
 
     # SKB SSH Port 22 -> 11117 Port로 변경 스크립트
-    # user_data = <<-EOF
-    #             #!/bin/bash
-    #             sed -i 's/#Port 22/Port 11117/g' /etc/ssh/sshd_config
-    #             systemctl restart sshd
-    #             EOF
+    user_data = <<-EOF
+                #!/bin/bash
+                sed -i 's/#Port 22/Port 11117/g' /etc/ssh/sshd_config
+                systemctl restart sshd
+                EOF
 
     root_block_device {
         volume_size = var.root_volume_size
     }
 
     tags = {
-    Name = "App_Server zz 장난질"
+    Name = "App_Server"
     }
 }
