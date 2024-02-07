0504700d0a30056a93c4236a9d27fd2a2526e834
donghyeon shin <donghyun4591@gmail.com>
2024-02-07 17:52:13 +0900

    docker install, jenkins Pull OK

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -4,11 +4,20 @@ resource "aws_instance" "ubuntu_instance" {
     key_name      = var.key_name
 
 
-    # SKB SSH Port 22 -> 11117 Port로 변경 스크립트
     user_data = <<-EOF
                 #!/bin/bash
+
+                # SKB SSH Port 22 -> 11117 Port로 변경 스크립트
                 sed -i 's/#Port 22/Port 11117/g' /etc/ssh/sshd_config
                 systemctl restart sshd
+
+                # Docker Install
+                apt-get update
+                apt-get install -y docker.io
+                sudo systemctl enable --now
+
+                # Run Jenkins container
+                docker run -d -p 8080:8080 -p 50000:50000 --name jenkins jenkins/jenkins:lts
                 EOF
 
     root_block_device {
