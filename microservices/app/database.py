16fc41c399e43ab3632204e907e2baf25c3fb291
donghyeon shin <donghyun4591@gmail.com>
2024-02-15 16:35:39 +0900

    dotenv, .env MySQL 접속 정보 환경변수 추가

--- microservices/app/database.py
+++ microservices/app/database.py
@@ -1,10 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+# .env 파일에서 환경 변수 로드
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# RDS MySQL 연결 정보 업데이트
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=True, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
